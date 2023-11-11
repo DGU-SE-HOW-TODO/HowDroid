bbc6cd00099587ac4ac8154a01d6a410ede6860d
l2zh <dlwnsgml0109@naver.com>
2023-11-11 22:09:40 +0900

    [init] 폴더링, 확장함수 추가

---
+++ app/src/main/java/com/example/howdroid/util/binding/BindingAdapter.kt
@@ -0,0 +1,3 @@
+package com.example.howdroid.util.binding
+
+object BindingAdapter
---
+++ app/src/main/java/com/example/howdroid/util/extension/ViewExt.kt
@@ -0,0 +1,27 @@
+package com.example.howdroid.util.extension
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+fun showSnackbar(view: View, message: String, isShort: Boolean = true) {
+    val duration = if (isShort) {
+        Snackbar.LENGTH_SHORT
+    } else {
+        Snackbar.LENGTH_LONG
+    }
+    Snackbar.make(view, message, duration).show()
+}
+
+inline fun View.setOnSingleClickListener(
+    delay: Long = 500L,
+    crossinline block: (View) -> Unit,
+) {
+    var previousClickedTime = 0L
+    setOnClickListener { view ->
+        val clickedTime = System.currentTimeMillis()
+        if (clickedTime - previousClickedTime >= delay) {
+            block(view)
+            previousClickedTime = clickedTime
+        }
+    }
+}
