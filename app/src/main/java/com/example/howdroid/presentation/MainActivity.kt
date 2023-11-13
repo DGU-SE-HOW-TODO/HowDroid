78c826be355326380e61d66587dba61174f4ac4a
l2zh <dlwnsgml0109@naver.com>
2023-11-13 21:01:44 +0900

    [init] MainActivity 생성

---
+++ app/src/main/java/com/example/howdroid/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.howdroid.presentation
+
+import android.os.Bundle
+import com.example.howdroid.R
+import com.example.howdroid.databinding.ActivityMainBinding
+import com.example.howdroid.util.binding.BindingActivity
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
