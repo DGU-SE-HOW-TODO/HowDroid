b5682a92a3242dba285bbf6cc1cd91c54ea33861
l2zh <dlwnsgml0109@naver.com>
2023-11-17 02:49:38 +0900

    [feat] #4 로그인,회원가입 뷰 작업 완료

--- app/src/main/java/com/example/howdroid/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/howdroid/presentation/login/LoginActivity.kt
@@ -3,11 +3,22 @@ package com.example.howdroid.presentation.login
 import android.os.Bundle
 import com.example.howdroid.R
 import com.example.howdroid.databinding.ActivityLoginBinding
+import com.example.howdroid.presentation.signup.SignUpActivity
 import com.example.howdroid.util.binding.BindingActivity
+import com.example.howdroid.util.extension.setOnSingleClickListener
+import com.example.howdroid.util.extension.startActivity
 
 class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        moveToSignUp()
+    }
+
+    private fun moveToSignUp() {
+        binding.tvLoginSignup.setOnSingleClickListener {
+            startActivity<SignUpActivity>()
+        }
     }
 }
