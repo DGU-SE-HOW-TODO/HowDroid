06e5b222a43e22377c9a525225be593a2884336f
l2zh <dlwnsgml0109@naver.com>
2023-11-16 17:36:37 +0900

    [add] #4 로그인 액티비티 추가

---
+++ app/src/main/java/com/example/howdroid/presentation/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.howdroid.presentation.login
+
+import android.os.Bundle
+import com.example.howdroid.R
+import com.example.howdroid.databinding.ActivityLoginBinding
+import com.example.howdroid.util.binding.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
