1b5e37caa4992c5acb72b600316328918a139eb6
Dani43 <eldms0406@naver.com>
2023-11-16 17:29:23 +0900

    [feat] #3 바텀 네비게이션 구현

---
+++ app/src/main/java/com/example/howdroid/presentation/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.example.howdroid.presentation
+
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.howdroid.R
+import com.example.howdroid.databinding.ActivityHomeBinding
+import com.example.howdroid.util.binding.BindingActivity
+
+class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initBottomNavigation()
+
+    }
+
+    private fun initBottomNavigation() {
+        val bnv = binding.bnvHome
+
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()
+        navController?.let {
+            it.setGraph(R.navigation.nav_graph)
+            bnv.setupWithNavController(it)
+        }
+    }
+}
