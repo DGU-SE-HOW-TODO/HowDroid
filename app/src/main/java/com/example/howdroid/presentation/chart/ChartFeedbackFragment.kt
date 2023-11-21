3127ca438a4f4f1476a28b4cb9aeaa2ca832a99c
Dani43 <eldms0406@naver.com>
2023-11-21 14:11:55 +0900

    [feat] #10 차트-통계 뷰 구현

---
+++ app/src/main/java/com/example/howdroid/presentation/chart/ChartFeedbackFragment.kt
@@ -0,0 +1,14 @@
+package com.example.howdroid.presentation.chart
+
+import android.os.Bundle
+import android.view.View
+import com.example.howdroid.R
+import com.example.howdroid.databinding.FragmentChartStatisticBinding
+import com.example.howdroid.util.binding.BindingFragment
+
+class ChartFeedbackFragment :
+    BindingFragment<FragmentChartStatisticBinding>(R.layout.fragment_chart_feedback) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
