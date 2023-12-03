352b42ee6723a98ecdd3503a9d6b707eec6e1206
Dani43 <eldms0406@naver.com>
2023-12-04 01:23:52 +0900

    [feat] #33 선택된 날짜 바텀  fragment로 보내기

--- app/src/main/java/com/example/howdroid/presentation/home/PutFailTagBottomSheetFragment.kt
+++ app/src/main/java/com/example/howdroid/presentation/home/PutFailTagBottomSheetFragment.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.example.howdroid.R
 import com.example.howdroid.databinding.FragmentBottomPutFaillTagBinding
+import com.example.howdroid.util.UiState
 import com.example.howdroid.util.binding.BindingBottomSheetDialogFragment
 import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,13 +20,15 @@ class PutFailTagBottomSheetFragment :
     BindingBottomSheetDialogFragment<FragmentBottomPutFaillTagBinding>(
         R.layout.fragment_bottom_put_faill_tag,
     ) {
-    // TODO 임시 데이터 삭제
-    private val mockMyFailTagList = listOf("잠", "정각병", "SNS", "게임")
     private val viewModel: PutFailTagViewModel by viewModels()
+    lateinit var myFailTagList: MutableList<String>
+    lateinit var selectedDate: String
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setMyFailTag()
+        selectedDate = arguments?.getString(SELECTED_DATE).toString()
+        viewModel.fetchMyFailTagList(selectedDate)
+
         addListeners()
         observeData()
         collectData()
@@ -33,7 +36,7 @@ class PutFailTagBottomSheetFragment :
 
     private fun setMyFailTag() {
         val inflater = LayoutInflater.from(requireContext())
-        for (failTagName in mockMyFailTagList) {
+        for (failTagName in myFailTagList) {
             val chip = inflater.inflate(
                 R.layout.view_fail_tag_chip,
                 binding.cgPutFailTag,
@@ -63,7 +66,7 @@ class PutFailTagBottomSheetFragment :
             if (group.checkedChipId != -1) {
                 val checkedChip = group.findViewById<Chip>(group.checkedChipId)
                 val checkedChipTitle = checkedChip.text.toString()
-                for (chipTitleWithoutHashTag in mockMyFailTagList) {
+                for (chipTitleWithoutHashTag in myFailTagList) {
                     if (checkedChipTitle.contains(chipTitleWithoutHashTag)) {
                         viewModel.setSelectedFailTag(chipTitleWithoutHashTag)
                     }
@@ -75,6 +78,16 @@ class PutFailTagBottomSheetFragment :
     }
 
     private fun collectData() {
+        viewModel.myFailTagList.flowWithLifecycle(lifecycle).onEach {
+            when (it) {
+                is UiState.Success -> {
+                    myFailTagList = it.data
+                    setMyFailTag()
+                }
+
+                else -> {}
+            }
+        }.launchIn(lifecycleScope)
         viewModel.selectedFailTag.flowWithLifecycle(lifecycle).onEach { selectedFailTag ->
             binding.btnCompletePutFailTag.isEnabled = selectedFailTag != null
             binding.cbPutFailTagDelayTillTomorrow.isEnabled = selectedFailTag != null
@@ -83,5 +96,9 @@ class PutFailTagBottomSheetFragment :
         viewModel.isDelayedTomorrow.flowWithLifecycle(lifecycle).onEach { isDelayedTomorrow ->
             binding.tvDelayedTomorrow.isSelected = isDelayedTomorrow
         }.launchIn(lifecycleScope)
+    }
+
+    companion object {
+        const val SELECTED_DATE = "selectedDate"
     }
 }
