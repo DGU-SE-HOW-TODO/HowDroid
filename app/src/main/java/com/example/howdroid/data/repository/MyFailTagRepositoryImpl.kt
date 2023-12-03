d1ecb8e978ae4a0985d4c10f071fb62924fd63ad
Dani43 <eldms0406@naver.com>
2023-12-04 01:22:15 +0900

    [feat] #33 나의 실패 태그 조회 api 연동

--- app/src/main/java/com/example/howdroid/data/repository/MyFailTagRepositoryImpl.kt
+++ app/src/main/java/com/example/howdroid/data/repository/MyFailTagRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.howdroid.data.repository
 
 import com.example.howdroid.data.datasource.remote.MyFailTagDataSource
 import com.example.howdroid.data.model.request.RequestMyFailTag
+import com.example.howdroid.data.model.response.ResponseMyFailTag
 import com.example.howdroid.domain.repository.MyFailTagRepository
 import javax.inject.Inject
 
@@ -11,5 +12,10 @@ class MyFailTagRepositoryImpl @Inject constructor(
     override suspend fun setMyFailTag(requestMyFailTag: RequestMyFailTag): Result<Unit> =
         runCatching {
             myFailTagDataSource.setMyFailTag(requestMyFailTag)
+        }
+
+    override suspend fun fetchMyFailTag(selectedDate: String): Result<ResponseMyFailTag> =
+        runCatching {
+            myFailTagDataSource.fetchMyFailTag(selectedDate)
         }
 }
--- app/src/main/java/com/example/howdroid/domain/repository/MyFailTagRepository.kt
+++ app/src/main/java/com/example/howdroid/domain/repository/MyFailTagRepository.kt
@@ -1,7 +1,9 @@
 package com.example.howdroid.domain.repository
 
 import com.example.howdroid.data.model.request.RequestMyFailTag
+import com.example.howdroid.data.model.response.ResponseMyFailTag
 
 interface MyFailTagRepository {
     suspend fun setMyFailTag(requestMyFailTag: RequestMyFailTag): Result<Unit>
+    suspend fun fetchMyFailTag(selectedDate: String): Result<ResponseMyFailTag>
 }
