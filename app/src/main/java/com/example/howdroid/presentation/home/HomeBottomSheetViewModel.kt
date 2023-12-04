2384c890d8b3647410d8daed47772fc2882d2934
l2zh <dlwnsgml0109@naver.com>
2023-12-05 03:03:11 +0900

    [fix] #27 todo 고정 이슈 해결

--- app/src/main/java/com/example/howdroid/presentation/home/HomeBottomSheetViewModel.kt
+++ app/src/main/java/com/example/howdroid/presentation/home/HomeBottomSheetViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.howdroid.presentation.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.howdroid.domain.repository.ToDoRepository
@@ -12,9 +14,18 @@ class HomeBottomSheetViewModel @Inject constructor(
     private val toDoRepository: ToDoRepository,
 ) : ViewModel() {
 
+    private val _fixToDo = MutableLiveData(false)
+    val fixToDo: LiveData<Boolean> get() = _fixToDo
+
     fun fixToDo(todoId: Long) {
         viewModelScope.launch {
             toDoRepository.fixToDo(todoId)
+                .onSuccess {
+                    _fixToDo.value = true
+                }
+                .onFailure {
+                    _fixToDo.value = false
+                }
         }
     }
 }
