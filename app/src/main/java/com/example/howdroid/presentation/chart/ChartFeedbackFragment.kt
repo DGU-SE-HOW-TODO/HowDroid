308a2f15c47af7b3b46b041ff678b4c5447d92d5
Dani43 <eldms0406@naver.com>
2023-12-05 02:04:27 +0900

    [feat] #40 통계 뷰 로직 작성 및 특정 부분 색상 지정

--- app/src/main/java/com/example/howdroid/presentation/chart/ChartFeedbackFragment.kt
+++ app/src/main/java/com/example/howdroid/presentation/chart/ChartFeedbackFragment.kt
@@ -2,12 +2,14 @@ package com.example.howdroid.presentation.chart
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.example.howdroid.R
 import com.example.howdroid.databinding.FragmentChartStatisticBinding
 import com.example.howdroid.util.binding.BindingFragment
 
 class ChartFeedbackFragment :
     BindingFragment<FragmentChartStatisticBinding>(R.layout.fragment_chart_feedback) {
+    private val viewModel: ChartViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
---
+++ app/src/main/java/com/example/howdroid/presentation/chart/ChartViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.howdroid.presentation.chart
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.howdroid.data.model.response.ResponseStatistic
+import com.example.howdroid.domain.repository.ChartRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.time.LocalDate
+import javax.inject.Inject
+
+@HiltViewModel
+class ChartViewModel @Inject constructor(
+    private val chartRepository: ChartRepository
+) : ViewModel() {
+    private val _preToDoAchievementRate = MutableStateFlow(0)
+    val preToDoAchievementRate get() = _preToDoAchievementRate.asStateFlow()
+    private val _nowToDoAchievementRate = MutableStateFlow(0)
+    val nowToDoAchievementRate get() = _nowToDoAchievementRate.asStateFlow()
+    private val _rateOfChange = MutableStateFlow(0)
+    val rateOfChange get() = _rateOfChange.asStateFlow()
+    private val _nowCategoryRateList =
+        MutableStateFlow<MutableList<ResponseStatistic.NowCategoryDate>>(
+            mutableListOf()
+        )
+    val nowCategoryRateList get() = _nowCategoryRateList.asStateFlow()
+    private val _nowBestCategory = MutableStateFlow("")
+    val nowBestCategory get() = _nowBestCategory.asStateFlow()
+    private val _nowFailTagList = MutableStateFlow<MutableList<ResponseStatistic.NowFailtag>>(
+        mutableListOf()
+    )
+    val nowFailTagList get() = _nowFailTagList.asStateFlow()
+    private val _nowWorstFailTag = MutableStateFlow("")
+    val nowWorstFailTag get() = _nowWorstFailTag.asStateFlow()
+
+    init {
+        fetchStatistic()
+    }
+
+    // TODO 초기화 유사한 거 끼리 함수화
+    private fun fetchStatistic() {
+        val today = LocalDate.now()
+        viewModelScope.launch {
+            chartRepository.fetchStatistic(today.toString())
+                .onSuccess { chartInfo ->
+                    val prePercent =
+                        chartInfo.prevTodoDoneCnt.toFloat() / chartInfo.prevTodoCnt.toFloat() * 100
+                    val nowPercent =
+                        chartInfo.nowTodoDoneCnt.toFloat() / chartInfo.nowTodoCnt.toFloat() * 100
+                    val totalPercent = prePercent + nowPercent
+                    _preToDoAchievementRate.value =
+                        (prePercent / totalPercent * 100).toInt()
+                    _nowToDoAchievementRate.value = (nowPercent / totalPercent * 100).toInt()
+                    _rateOfChange.value = chartInfo.rateOfChange
+                    _nowCategoryRateList.value = chartInfo.nowCategoryDate.toMutableList()
+                    _nowFailTagList.value = chartInfo.nowFailtagList.toMutableList()
+                    _nowBestCategory.value = chartInfo.nowBestCategory
+                    _nowWorstFailTag.value = chartInfo.nowWorstFailtag
+                }
+                .onFailure { throwable ->
+                    Timber.e(throwable.message)
+                }
+        }
+    }
+
+}
