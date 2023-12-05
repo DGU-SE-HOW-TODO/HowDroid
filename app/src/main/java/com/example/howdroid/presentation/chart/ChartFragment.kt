8af35f6eda0f67fa9ab02ae4780c5d8e1d8c3d27
Dani43 <eldms0406@naver.com>
2023-12-05 12:57:58 +0900

    [feat] #40 통계/피드백 월 주차 디자인 및 구현

--- app/src/main/java/com/example/howdroid/presentation/chart/ChartFragment.kt
+++ app/src/main/java/com/example/howdroid/presentation/chart/ChartFragment.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.example.howdroid.R
 import com.example.howdroid.databinding.FragmentChartBinding
 import com.example.howdroid.util.binding.BindingFragment
 import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
+@AndroidEntryPoint
 class ChartFragment : BindingFragment<FragmentChartBinding>(R.layout.fragment_chart) {
     private val viewModel: ChartViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setChartPagerAdapter()
+        collectData()
     }
 
     private fun setChartPagerAdapter() {
@@ -31,6 +38,15 @@ class ChartFragment : BindingFragment<FragmentChartBinding>(R.layout.fragment_ch
                 1 -> tab.text = getString(R.string.chart_feedback_title)
             }
         }.attach()
+    }
+
+    private fun collectData() {
+        viewModel.feedBackContent.flowWithLifecycle(lifecycle).onEach { feedBackContent ->
+            if (feedBackContent != null) {
+                binding.tvChartTitle.text =
+                    String.format("%d월 %d주차 통계/피드백", feedBackContent.month, feedBackContent.week)
+            }
+        }.launchIn(lifecycleScope)
     }
 
 //    override fun onDestroyView() {
