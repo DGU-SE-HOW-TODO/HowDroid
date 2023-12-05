fcbee1117fa01a10f501261c9eb950dc7cf35b88
Dani43 <eldms0406@naver.com>
2023-12-06 00:57:00 +0900

    [mod] #40 피드백 텍스트 관련 자료형 nullable로 수정

--- app/src/main/java/com/example/howdroid/data/model/response/ResponseFeedBack.kt
+++ app/src/main/java/com/example/howdroid/data/model/response/ResponseFeedBack.kt
@@ -6,13 +6,13 @@ import kotlinx.serialization.Serializable
 data class ResponseFeedBack(
     val month: Int,
     val week: Int,
-    val delayDetailMessage: String,
-    val delayMessage: String,
+    val delayDetailMessage: String?,
+    val delayMessage: String?,
     val firstPriPercent: Int,
-    val priorityDetailMessage: String,
-    val priorityMessage: String,
-    val rateDetailMessage: String,
-    val rateMessage: String,
+    val priorityDetailMessage: String?,
+    val priorityMessage: String?,
+    val rateDetailMessage: String?,
+    val rateMessage: String?,
     val secondPriPercent: Int,
     val thirdPriPercent: Int
 )
